abc5546d618add9500d9679bdbdfb50582de4b9a
Developer <gupta.nivrit@gmail.com>
2020-04-16 18:00:31 +0530

    implementation of UICollectionView ,custom cell , fetch data from server and showing  on UICollection View

---
+++ Github-Pro/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,33 @@
+//
+//  GFAvatarImageView.swift
+//  Github-Pro
+//
+//  Created by Developer on 16/04/2020.
+//  Copyright © 2020 Developer. All rights reserved.
+//
+
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius  = 10
+        clipsToBounds       = true
+        image               = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
--- Github-Pro/screens/FollowersListVC.swift
+++ Github-Pro/screens/FollowersListVC.swift
@@ -8,49 +8,102 @@
 
 import UIKit
 
-
-
 class FollowersListVC: UIViewController {
+    
+    enum Section {
+        case main
+    }
+    
+    var followers:[Follower] = []
+    
     var username:String!
-
-
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section,Follower>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       navigationController?.navigationBar.prefersLargeTitles = true
-        view.backgroundColor = .systemBackground
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+        configureDataSource()
+
+    }
+    
+       override func viewWillAppear(_ animated: Bool) {
+         super.viewWillAppear(animated)
+   
+           navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func configureViewController(){
         
-  
+        navigationController?.navigationBar.prefersLargeTitles = true
+         view.backgroundColor = .systemBackground
+
+    }
+
+     func configureCollectionView(){
+        
+        
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColoumnFlowLayout())
+        view.addSubview(collectionView)
+        
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func createThreeColoumnFlowLayout() -> UICollectionViewFlowLayout {
+        
+        
+           let width                       = view.bounds.width
+           let padding: CGFloat            = 12
+           let minimumItemSpacing: CGFloat = 10
+           let availableWidth              = width - (padding * 2) - (minimumItemSpacing * 2)
+           let itemWidth                   = availableWidth / 3
+           
+           let flowLayout                  = UICollectionViewFlowLayout()
+           flowLayout.sectionInset         = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+           flowLayout.itemSize             = CGSize(width: itemWidth, height: itemWidth + 40)
+           
+           return flowLayout
+        
+    }
+    
+    func getFollowers(){
         
         NetworkManager.shared.getFollowers(for: username, page: 1) { (result) in
-            
-            
             switch result {
             case .success(let followers):
                 
-                print("followers.count  \(followers.count)")
-                print(followers)
-            
+                self.followers = followers
+                print(self.followers)
+                self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message:error.rawValue, buttonTitle: "Ok")
 
             }
         }
-
     }
     
-
-       override func viewWillAppear(_ animated: Bool) {
-         super.viewWillAppear(animated)
-   
-           navigationController?.setNavigationBarHidden(false, animated: true)
+    func configureDataSource(){
         
-        
+        dataSource = UICollectionViewDiffableDataSource<Section,Follower>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, follower) -> UICollectionViewCell? in
+             
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            
+            cell.set(follower: follower)
+            
+            return cell
+        })
     }
-    
-    
 
-     
-
-
+        func updateData(){
+        var snapShop = NSDiffableDataSourceSnapshot<Section,Follower>()
+        snapShop.appendSections([.main])   // know about the sections
+        snapShop.appendItems(followers)
+        
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapShop, animatingDifferences: true)}
+    }
 }
