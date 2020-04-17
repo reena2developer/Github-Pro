345de7067377950f746e03ed495b8ce1f4a01ae5
Developer <gupta.nivrit@gmail.com>
2020-04-17 17:40:40 +0530

    implement pagination

--- Github-Pro/screens/FollowersListVC.swift
+++ Github-Pro/screens/FollowersListVC.swift
@@ -13,6 +13,9 @@ class FollowersListVC: UIViewController {
     enum Section {case main}
     
     var followers:[Follower] = []
+    var page = 1
+    var hasMoreFollower = true
+    
     
     var username:String!
     var collectionView: UICollectionView!
@@ -23,7 +26,7 @@ class FollowersListVC: UIViewController {
         
         configureViewController()
         configureCollectionView()
-        getFollowers()
+        getFollowers(username: username, page: page)
         configureDataSource()
 
     }
@@ -46,24 +49,24 @@ class FollowersListVC: UIViewController {
         
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColoumnFlowLayout(in: view))
         view.addSubview(collectionView)
-        
+        collectionView.delegate = self
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
  
     
-    func getFollowers(){
+    func getFollowers(username:String,page:Int){
         
         // very imporant  for the memory leak we need to make the self as a weak veriable but anything that is weak its going  to be optional , [weak self called the capture list]
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] (result) in
+        NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] (result) in
             
             guard let self = self else {return}
             switch result {
             case .success(let followers):
-                
-                self.followers = followers
+                if followers.count < 100 {self.hasMoreFollower = false}
+                self.followers.append(contentsOf: followers)
                 self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message:error.rawValue, buttonTitle: "Ok")
@@ -91,5 +94,27 @@ class FollowersListVC: UIViewController {
         
         DispatchQueue.main.async {
             self.dataSource.apply(snapShop, animatingDifferences: true)}
+    }
+}
+
+
+extension FollowersListVC: UICollectionViewDelegate{
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let offSetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.size.height
+        
+        
+        if offSetY > contentHeight - height {
+            guard hasMoreFollower else {return}
+            page += 1
+
+            getFollowers(username: username, page: page)
+        }
+
+        
+        
+        
     }
 }
