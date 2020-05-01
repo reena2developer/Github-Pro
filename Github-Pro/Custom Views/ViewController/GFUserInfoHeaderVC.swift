b80f774121e1245f2dc3c81de217c131bf0b8ad6
Developer <gupta.nivrit@gmail.com>
2020-05-01 16:02:34 +0530

    Refactor code  for AvatarImageView

--- Github-Pro/Custom Views/ViewController/GFUserInfoHeaderVC.swift
+++ Github-Pro/Custom Views/ViewController/GFUserInfoHeaderVC.swift
@@ -39,7 +39,8 @@ class GFUserInfoHeaderVC: UIViewController {
     
     func configureUIElements(){
         
-        avatarImageView.downloadImage(from: user.avatarUrl)
+        downloadAvatarImage()
+     
         usernameLabel.text = user.login
         nameLabel.text = user.name ?? ""
         locationLabel.text = user.location ?? "No location"
@@ -49,6 +50,16 @@ class GFUserInfoHeaderVC: UIViewController {
         locationImageView.image = UIImage(systemName: SFSymbols.location)
         locationImageView.tintColor = .secondaryLabel
         
+    }
+    
+    func downloadAvatarImage(){
+        
+        NetworkManager.shared.downloadImage(from: user.avatarUrl) { [weak self] image in
+                 guard let self  = self else {return}
+                 DispatchQueue.main.async {
+                     self.avatarImageView.image = image
+                 }
+             }
     }
 
     
