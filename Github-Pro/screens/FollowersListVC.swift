affd0c6d54b5a6dbcc788550a5d6f9244c8d0401
Developer <gupta.nivrit@gmail.com>
2020-05-14 12:36:13 +0530

    small bug  fixed in followersList

--- Github-Pro/screens/FollowersListVC.swift
+++ Github-Pro/screens/FollowersListVC.swift
@@ -249,7 +249,8 @@ extension FollowersListVC:followerListVCDelegate{
         page          = 1
         followers.removeAll()
         filteredFollowers.removeAll()
-        collectionView.setContentOffset(.zero, animated: true)
+      //  collectionView.setContentOffset(.zero, animated: true)
+        collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .top, animated: true)
         getFollowers(username: username, page: page)
         
     }
