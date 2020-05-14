225ac52eb96ed3c666200102d029da1c422f3e60
Developer <gupta.nivrit@gmail.com>
2020-05-14 12:25:06 +0530

    refactor empty  state view code

--- Github-Pro/Custom Views/Views/GFEmptyStateView.swift
+++ Github-Pro/Custom Views/Views/GFEmptyStateView.swift
@@ -32,22 +32,37 @@ class GFEmptyStateView: UIView {
     
     private func configure(){
         
+     configureLogoImageView()
+     configureMessagelabel()
+        
+    }
+    
+    private func configureMessagelabel(){
         addSubview(messageLabel)
-        addSubview(logoImageVIew)
-        
-        
         messageLabel.numberOfLines = 3
         messageLabel.textColor     =  .secondaryLabel
-        logoImageVIew.image  =  Images.emptyStateLogo
-        logoImageVIew.translatesAutoresizingMaskIntoConstraints = false
         
         // condition  for checking the constraint for device like iphoneSE
-        let labelCenterYConstant:CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? -80 : -150
+              let labelCenterYConstant:CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? -80 : -150
+              let messaageLabelCenterYConstraint =  messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: labelCenterYConstant)
+              
+              messaageLabelCenterYConstraint.isActive = true
         
-        let messaageLabelCenterYConstraint =  messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: labelCenterYConstant)
+        NSLayoutConstraint.activate([
+           
+               messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+               messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+               messageLabel.heightAnchor.constraint(equalToConstant: 200),
+           ])
+              
         
-        messaageLabelCenterYConstraint.isActive = true
-        
+    }
+    
+    
+    private func configureLogoImageView(){
+        addSubview(logoImageVIew)
+        logoImageVIew.image  =  Images.emptyStateLogo
+        logoImageVIew.translatesAutoresizingMaskIntoConstraints = false
         
         // condition  for checking the constraint for device like iphoneSE
            let logoBottomConstant:CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? 80 : 40
@@ -58,12 +73,7 @@ class GFEmptyStateView: UIView {
         
         
         NSLayoutConstraint.activate([
-        
-            messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
-            messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
-            messageLabel.heightAnchor.constraint(equalToConstant: 200),
-            
-            
+
             logoImageVIew.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageVIew.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageVIew.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
